dd42f1099d0dd2bb8d22cfd0120786a4edd09c2f
Mhmod <mahmoud97787@gmail.com>
2024-08-28 17:09:07 +0300

    add assets and utils files

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,117 @@
+// ignore_for_file: prefer_single_quotes
+class AppImages {
+  AppImages._();
+  
+  /// Assets for imagesColorsColorBlack
+  /// assets/images/colors/color_black.png
+  static const String imagesColorsColorBlack = "assets/images/colors/color_black.png";
+
+  /// Assets for imagesColorsColorBrown
+  /// assets/images/colors/color_brown.png
+  static const String imagesColorsColorBrown = "assets/images/colors/color_brown.png";
+
+  /// Assets for imagesColorsColorDustyYellow
+  /// assets/images/colors/color_dusty_yellow.png
+  static const String imagesColorsColorDustyYellow = "assets/images/colors/color_dusty_yellow.png";
+
+  /// Assets for imagesColorsColorGray
+  /// assets/images/colors/color_gray.png
+  static const String imagesColorsColorGray = "assets/images/colors/color_gray.png";
+
+  /// Assets for imagesColorsColorGreen
+  /// assets/images/colors/color_green.png
+  static const String imagesColorsColorGreen = "assets/images/colors/color_green.png";
+
+  /// Assets for imagesColorsColorRed
+  /// assets/images/colors/color_red.png
+  static const String imagesColorsColorRed = "assets/images/colors/color_red.png";
+
+  /// Assets for imagesColorsColorWhite
+  /// assets/images/colors/color_white.png
+  static const String imagesColorsColorWhite = "assets/images/colors/color_white.png";
+
+  /// Assets for imagesColorsYellow
+  /// assets/images/colors/yellow.png
+  static const String imagesColorsYellow = "assets/images/colors/yellow.png";
+
+  /// Assets for imagesFamilyMembersFamilyDaughter
+  /// assets/images/family_members/family_daughter.png
+  static const String imagesFamilyMembersFamilyDaughter = "assets/images/family_members/family_daughter.png";
+
+  /// Assets for imagesFamilyMembersFamilyFather
+  /// assets/images/family_members/family_father.png
+  static const String imagesFamilyMembersFamilyFather = "assets/images/family_members/family_father.png";
+
+  /// Assets for imagesFamilyMembersFamilyGrandfather
+  /// assets/images/family_members/family_grandfather.png
+  static const String imagesFamilyMembersFamilyGrandfather = "assets/images/family_members/family_grandfather.png";
+
+  /// Assets for imagesFamilyMembersFamilyGrandmother
+  /// assets/images/family_members/family_grandmother.png
+  static const String imagesFamilyMembersFamilyGrandmother = "assets/images/family_members/family_grandmother.png";
+
+  /// Assets for imagesFamilyMembersFamilyMother
+  /// assets/images/family_members/family_mother.png
+  static const String imagesFamilyMembersFamilyMother = "assets/images/family_members/family_mother.png";
+
+  /// Assets for imagesFamilyMembersFamilyOlderBrother
+  /// assets/images/family_members/family_older_brother.png
+  static const String imagesFamilyMembersFamilyOlderBrother = "assets/images/family_members/family_older_brother.png";
+
+  /// Assets for imagesFamilyMembersFamilyOlderSister
+  /// assets/images/family_members/family_older_sister.png
+  static const String imagesFamilyMembersFamilyOlderSister = "assets/images/family_members/family_older_sister.png";
+
+  /// Assets for imagesFamilyMembersFamilySon
+  /// assets/images/family_members/family_son.png
+  static const String imagesFamilyMembersFamilySon = "assets/images/family_members/family_son.png";
+
+  /// Assets for imagesFamilyMembersFamilyYoungerBrother
+  /// assets/images/family_members/family_younger_brother.png
+  static const String imagesFamilyMembersFamilyYoungerBrother = "assets/images/family_members/family_younger_brother.png";
+
+  /// Assets for imagesFamilyMembersFamilyYoungerSister
+  /// assets/images/family_members/family_younger_sister.png
+  static const String imagesFamilyMembersFamilyYoungerSister = "assets/images/family_members/family_younger_sister.png";
+
+  /// Assets for imagesNumbersNumberEight
+  /// assets/images/numbers/number_eight.png
+  static const String imagesNumbersNumberEight = "assets/images/numbers/number_eight.png";
+
+  /// Assets for imagesNumbersNumberFive
+  /// assets/images/numbers/number_five.png
+  static const String imagesNumbersNumberFive = "assets/images/numbers/number_five.png";
+
+  /// Assets for imagesNumbersNumberFour
+  /// assets/images/numbers/number_four.png
+  static const String imagesNumbersNumberFour = "assets/images/numbers/number_four.png";
+
+  /// Assets for imagesNumbersNumberNine
+  /// assets/images/numbers/number_nine.png
+  static const String imagesNumbersNumberNine = "assets/images/numbers/number_nine.png";
+
+  /// Assets for imagesNumbersNumberOne
+  /// assets/images/numbers/number_one.png
+  static const String imagesNumbersNumberOne = "assets/images/numbers/number_one.png";
+
+  /// Assets for imagesNumbersNumberSeven
+  /// assets/images/numbers/number_seven.png
+  static const String imagesNumbersNumberSeven = "assets/images/numbers/number_seven.png";
+
+  /// Assets for imagesNumbersNumberSix
+  /// assets/images/numbers/number_six.png
+  static const String imagesNumbersNumberSix = "assets/images/numbers/number_six.png";
+
+  /// Assets for imagesNumbersNumberTen
+  /// assets/images/numbers/number_ten.png
+  static const String imagesNumbersNumberTen = "assets/images/numbers/number_ten.png";
+
+  /// Assets for imagesNumbersNumberThree
+  /// assets/images/numbers/number_three.png
+  static const String imagesNumbersNumberThree = "assets/images/numbers/number_three.png";
+
+  /// Assets for imagesNumbersNumberTwo
+  /// assets/images/numbers/number_two.png
+  static const String imagesNumbersNumberTwo = "assets/images/numbers/number_two.png";
+}
+
---
+++ lib/utils/app_sounds.dart
@@ -0,0 +1,153 @@
+// ignore_for_file: prefer_single_quotes
+class AppSounds {
+  AppSounds._();
+  
+  /// Assets for soundsColorsBlack
+  /// assets/sounds/colors/black.wav
+  static const String soundsColorsBlack = "assets/sounds/colors/black.wav";
+
+  /// Assets for soundsColorsBrown
+  /// assets/sounds/colors/brown.wav
+  static const String soundsColorsBrown = "assets/sounds/colors/brown.wav";
+
+  /// Assets for soundsColorsDusty yellow
+  /// assets/sounds/colors/dusty yellow.wav
+  static const String soundsColorsDustyYellow = "assets/sounds/colors/dusty yellow.wav";
+
+  /// Assets for soundsColorsGray
+  /// assets/sounds/colors/gray.wav
+  static const String soundsColorsGray = "assets/sounds/colors/gray.wav";
+
+  /// Assets for soundsColorsGreen
+  /// assets/sounds/colors/green.wav
+  static const String soundsColorsGreen = "assets/sounds/colors/green.wav";
+
+  /// Assets for soundsColorsRed
+  /// assets/sounds/colors/red.wav
+  static const String soundsColorsRed = "assets/sounds/colors/red.wav";
+
+  /// Assets for soundsColorsWhite
+  /// assets/sounds/colors/white.wav
+  static const String soundsColorsWhite = "assets/sounds/colors/white.wav";
+
+  /// Assets for soundsColorsYellow
+  /// assets/sounds/colors/yellow.wav
+  static const String soundsColorsYellow = "assets/sounds/colors/yellow.wav";
+
+  /// Assets for soundsFamilyMembersDaughter
+  /// assets/sounds/family_members/daughter.wav
+  static const String soundsFamilyMembersDaughter = "assets/sounds/family_members/daughter.wav";
+
+  /// Assets for soundsFamilyMembersFather
+  /// assets/sounds/family_members/father.wav
+  static const String soundsFamilyMembersFather = "assets/sounds/family_members/father.wav";
+
+  /// Assets for soundsFamilyMembersGrand father
+  /// assets/sounds/family_members/grand father.wav
+  static const String soundsFamilyMembersGrandFather = "assets/sounds/family_members/grand father.wav";
+
+  /// Assets for soundsFamilyMembersGrand mother
+  /// assets/sounds/family_members/grand mother.wav
+  static const String soundsFamilyMembersGrandMother = "assets/sounds/family_members/grand mother.wav";
+
+  /// Assets for soundsFamilyMembersMother
+  /// assets/sounds/family_members/mother.wav
+  static const String soundsFamilyMembersMother = "assets/sounds/family_members/mother.wav";
+
+  /// Assets for soundsFamilyMembersOlder bother
+  /// assets/sounds/family_members/older bother.wav
+  static const String soundsFamilyMembersOlderBother = "assets/sounds/family_members/older bother.wav";
+
+  /// Assets for soundsFamilyMembersOlder sister
+  /// assets/sounds/family_members/older sister.wav
+  static const String soundsFamilyMembersOlderSister = "assets/sounds/family_members/older sister.wav";
+
+  /// Assets for soundsFamilyMembersSon
+  /// assets/sounds/family_members/son.wav
+  static const String soundsFamilyMembersSon = "assets/sounds/family_members/son.wav";
+
+  /// Assets for soundsFamilyMembersYounger brohter
+  /// assets/sounds/family_members/younger brohter.wav
+  static const String soundsFamilyMembersYoungerBrother = "assets/sounds/family_members/younger brohter.wav";
+
+  /// Assets for soundsFamilyMembersYounger sister
+  /// assets/sounds/family_members/younger sister.wav
+  static const String soundsFamilyMembersYoungerSister = "assets/sounds/family_members/younger sister.wav";
+
+  /// Assets for soundsNumbersNumberEightSound
+  /// assets/sounds/numbers/number_eight_sound.mp3
+  static const String soundsNumbersNumberEightSound = "assets/sounds/numbers/number_eight_sound.mp3";
+
+  /// Assets for soundsNumbersNumberFiveSound
+  /// assets/sounds/numbers/number_five_sound.mp3
+  static const String soundsNumbersNumberFiveSound = "assets/sounds/numbers/number_five_sound.mp3";
+
+  /// Assets for soundsNumbersNumberFourSound
+  /// assets/sounds/numbers/number_four_sound.mp3
+  static const String soundsNumbersNumberFourSound = "assets/sounds/numbers/number_four_sound.mp3";
+
+  /// Assets for soundsNumbersNumberNineSound
+  /// assets/sounds/numbers/number_nine_sound.mp3
+  static const String soundsNumbersNumberNineSound = "assets/sounds/numbers/number_nine_sound.mp3";
+
+  /// Assets for soundsNumbersNumberOneSound
+  /// assets/sounds/numbers/number_one_sound.mp3
+  static const String soundsNumbersNumberOneSound = "assets/sounds/numbers/number_one_sound.mp3";
+
+  /// Assets for soundsNumbersNumberSevenSound
+  /// assets/sounds/numbers/number_seven_sound.mp3
+  static const String soundsNumbersNumberSevenSound = "assets/sounds/numbers/number_seven_sound.mp3";
+
+  /// Assets for soundsNumbersNumberSixSound
+  /// assets/sounds/numbers/number_six_sound.mp3
+  static const String soundsNumbersNumberSixSound = "assets/sounds/numbers/number_six_sound.mp3";
+
+  /// Assets for soundsNumbersNumberTenSound
+  /// assets/sounds/numbers/number_ten_sound.mp3
+  static const String soundsNumbersNumberTenSound = "assets/sounds/numbers/number_ten_sound.mp3";
+
+  /// Assets for soundsNumbersNumberThreeSound
+  /// assets/sounds/numbers/number_three_sound.mp3
+  static const String soundsNumbersNumberThreeSound = "assets/sounds/numbers/number_three_sound.mp3";
+
+  /// Assets for soundsNumbersNumberTwoSound
+  /// assets/sounds/numbers/number_two_sound.mp3
+  static const String soundsNumbersNumberTwoSound = "assets/sounds/numbers/number_two_sound.mp3";
+
+  /// Assets for soundsPhrasesAreYouComing
+  /// assets/sounds/phrases/are_you_coming.wav
+  static const String soundsPhrasesAreYouComing = "assets/sounds/phrases/are_you_coming.wav";
+
+  /// Assets for soundsPhrasesDontForgetToSubscribe
+  /// assets/sounds/phrases/dont_forget_to_subscribe.wav
+  static const String soundsPhrasesDontForgetToSubscribe = "assets/sounds/phrases/dont_forget_to_subscribe.wav";
+
+  /// Assets for soundsPhrasesHowAreYouFeeling
+  /// assets/sounds/phrases/how_are_you_feeling.wav
+  static const String soundsPhrasesHowAreYouFeeling = "assets/sounds/phrases/how_are_you_feeling.wav";
+
+  /// Assets for soundsPhrasesILoveAnime
+  /// assets/sounds/phrases/i_love_anime.wav
+  static const String soundsPhrasesILoveAnime = "assets/sounds/phrases/i_love_anime.wav";
+
+  /// Assets for soundsPhrasesILoveProgramming
+  /// assets/sounds/phrases/i_love_programming.wav
+  static const String soundsPhrasesILoveProgramming = "assets/sounds/phrases/i_love_programming.wav";
+
+  /// Assets for soundsPhrasesProgrammingIsEasy
+  /// assets/sounds/phrases/programming_is_easy.wav
+  static const String soundsPhrasesProgrammingIsEasy = "assets/sounds/phrases/programming_is_easy.wav";
+
+  /// Assets for soundsPhrasesWhatIsYourName
+  /// assets/sounds/phrases/what_is_your_name.wav
+  static const String soundsPhrasesWhatIsYourName = "assets/sounds/phrases/what_is_your_name.wav";
+
+  /// Assets for soundsPhrasesWhereAreYouGoing
+  /// assets/sounds/phrases/where_are_you_going.wav
+  static const String soundsPhrasesWhereAreYouGoing = "assets/sounds/phrases/where_are_you_going.wav";
+
+  /// Assets for soundsPhrasesYesImComing
+  /// assets/sounds/phrases/yes_im_coming.wav
+  static const String soundsPhrasesYesImComing = "assets/sounds/phrases/yes_im_coming.wav";
+}
+
