6d1ada9803058226f6a04ff9215577110e88e84b
Mhmod <mahmoud97787@gmail.com>
2024-08-28 22:16:26 +0300

    create item model

---
+++ lib/models/item_model.dart
@@ -0,0 +1,13 @@
+class ItemModel {
+  final String jpName;
+  final String enName;
+  final String sound;
+  final String img;
+
+  const ItemModel({
+    required this.jpName,
+    required this.enName,
+    required this.sound,
+    required this.img,
+  });
+}
