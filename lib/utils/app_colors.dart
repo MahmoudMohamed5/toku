f61f8b7f7d1cae6aa8a9a72df3a75f1fcbfcb0e4
Mhmod <mahmoud97787@gmail.com>
2024-08-28 19:42:19 +0300

    add colors

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color brownColor = Color(0xff49332A);
+  static const Color orangeColor = Color(0xffF29130);
+  static const Color greenColor = Color(0xff528032);
+  static const Color purpleColor = Color(0xff7B3F9F);
+  static const Color blueColor = Color(0xff47A5CB);
+}
